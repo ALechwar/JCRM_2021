5d403afee6d59edacd2f40ea995f3ba7c4c18d5e
ALechwar <92215114+ALechwar@users.noreply.github.com>
2022-03-09 19:17:40 +0100

    Update trend.R

--- trend.R
+++ trend.R
@@ -4,42 +4,30 @@ library("lme4")
 shapiro.test(trend$spue) #normality check
 hist(trend$spue)
 
-mod<- glm(spue~year+poly(month, 2), data = trend, family = poisson) #investigating quadratic relationship between spue and month
-summary(mod) #no quadratic relationship 
-
-mod1<- glm(spue~year+as.factor(month), data = trend, family = poisson) #glm
-summary(mod1)
+model.1 <- glm(`number of minke whale sightings` ~ year + as.factor(season), offset = log(`number of surveys`), family = poisson(link = "log"), data = trend)
+summary(model.1) #overdispersion in the data
 
 par(mfrow=c(2,2)) #checking if data meets model assumptions
-plot(mod1)
+plot(model.1)
 
+
+model.2 <- glm(`number of minke whale sightings` ~ year + as.factor(season), offset = log(`number of surveys`), family = quasipoisson(link = "log"), data = trend)
+summary(model.2)
 
 #graphs 
 year_labels<-c('2008','2009', '2010', '2011','2012','2013','2014','2015','2016','2017','2018') #custom labels for x axis
-trend$month<-as.character(trend$month)
-trend$month_title<-trend$month
-
-#change month names from numbric for the legend 
-trend['month_title'][trend['month_title']=='4']<-'April'
-trend['month_title'][trend['month_title']=='5']<-'May'
-trend['month_title'][trend['month_title']=='6']<-'June'
-trend['month_title'][trend['month_title']=='7']<-'July'
-trend['month_title'][trend['month_title']=='8']<-'August'
-trend['month_title'][trend['month_title']=='9']<-'September'
-trend['month_title'][trend['month_title']=='10']<-'October'
-trend['month_title'][trend['month_title']=='11']<-'November'
-
-#calculate the points for trendline
-x<-seq(1:10)
-y<-plogis(coef(mod1)[1]+coef(mod1)[2]*x)  
+trend$season<-as.character(trend$season)
+trend$season_title<-trend$season
 
 #create plot
-ggplot(trend, aes(x = year, y = spue, shape=month)) + 
+x<-seq(1:10)
+y<-plogis(coef(model.1)[1]+coef(model.1)[2]*x) 
+
+ggplot(trend, aes(x = year, y = spue, shape=season)) + 
   geom_point(size = 2) +  
   theme_bw()+
   labs(x='Year', y='Sightings per unit effort', fill='Month')+
   scale_x_continuous(labels=year_labels, breaks=seq(0, 10, by=1))+
-  geom_segment(aes(x=1,y=0.5867881,xend=10,yend=0.3334756), color='black') +
-  scale_shape_manual(values=1:8, name='Month', 
-                   labels=c("April", 'May', 'June', 'July', 'July', 'August', 'September',"October", "November"))
-
+  geom_segment(aes(x=1,y=0.6798772,xend=10,yend=0.41943591), color='black') +
+  scale_shape_manual(values=1:4, name='Month', 
+                     labels=c("April-May", 'June-July', 'August-September', "October-November"))
